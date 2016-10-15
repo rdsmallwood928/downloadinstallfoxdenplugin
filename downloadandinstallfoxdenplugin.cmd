3dda0f57a0b7ab598e25ac380029438a05cce025
Robert <rdsmallwood928@gmail.com>
2016-10-14 20:21:47 -0600

    first commit

---
+++ downloadandinstallfoxdenplugin.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+echo var WinHttpReq = new ActiveXObject("WinHttp.WinHttpRequest.5.1"); > wget.js
+echo WinHttpReq.Open("GET", WScript.Arguments(0), /*async=*/false); >> wget.js
+echo WinHttpReq.Send(); >> wget.js
+echo BinStream = new ActiveXObject("ADODB.Stream"); >> wget.js
+echo BinStream.Type = 1; >> wget.js
+echo BinStream.Open(); >> wget.js
+echo BinStream.Write(WinHttpReq.ResponseBody); >> wget.js
+echo BinStream.SaveToFile("out.msi"); >> wget.js
+
+cscript wget.js https://alpha.foxden.io/plugins/foxden/fdaudiovideoplugin.msi
+
+msiexec /i out.msi /quiet /qn /norestart
+
+del wget.js
+del out.msi
